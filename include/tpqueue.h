6b0319181dd86a6b5b585104102456db905552a2
Kstovchanin1902 <78904695+Kstovchanin1902@users.noreply.github.com>
2021-05-11 08:30:04 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -43,46 +43,64 @@ TPQueue<T>::~TPQueue()
 template<typename T>
 void TPQueue<T>::push(const T & item)
 {
-    // проверяем, ести ли свободное место в очереди
+     // проверяем, ести ли свободное место в очереди
     assert( count < size ); 
     int prior = 0;
     int beginC = begin;
     int inPlace = 0;
+    bool flag = 0;
     T trans, trans2;
-    while (beginC != end)
+    std::cout << "1" << std::endl;
+    std::cout << "begin = " << begin << " end = " << end << std::endl;
+    if (beginC != end)
     {
-        if (arr[beginC].prior < item.prior)
+        while (beginC != end)
         {
-            trans = arr[beginC];
-            arr[beginC] = item;
-            inPlace = beginC;
-            end++;
+            std::cout << "jkAl" << std::endl;
+            std::cout << "count = " << count << std::endl;
+            if (!(arr[beginC].prior < item.prior))
+            {
+                beginC++;
+                if (beginC == size)
+                    beginC = 0;
+            }
+            else
+            {
+                trans = arr[beginC];
+                arr[beginC] = item;
+                inPlace = beginC + 1;
+                end++;
+                count++;
+                flag = 1;
+                break;
+            }
+        }
+        beginC = inPlace;
+        if (flag)
+        {
+            while (beginC != end)
+            {
+                trans2 = arr[beginC];
+                arr[beginC] = trans;
+                trans = trans2;
+                beginC++;
+                if (beginC == size)
+                    beginC = 0;
+
+            }
+        }
+        if (!flag)
+        {
+            arr[end++] = item;
+            inPlace = end;
             count++;
-            break;
-        }
-        if (beginC + 1 == size)
-        {
-            beginC = 0;
-        }
-        else
-        {
-            beginC++;
         }
     }
-    beginC = inPlace + 1;
-    while (beginC != end)
+    else
     {
-       trans2 = arr[beginC];
-       arr[beginC] = trans;
-       trans = trans2;
-       if (beginC + 1 == size)
-       {
-           beginC = 0;
-       }
-       else
-       {
-           beginC++;
-       }
+        arr[begin] = item;
+        count++;
+        end++;
     }
  
     // проверка кругового заполнения очереди
