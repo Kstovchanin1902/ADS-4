7593f0d4035f2ad9bc9fd40ab61ad036db1c1ab7
Kstovchanin1902 <78904695+Kstovchanin1902@users.noreply.github.com>
2021-05-12 21:31:10 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -8,8 +8,7 @@ class TPQueue {
 private:
    T *arr;
    int size;
-   int begin,
-       end;
+   int begin, end;
    int count;
 public:
    TQueue(int =100);
@@ -32,65 +31,65 @@ TPQueue<T>::~TPQueue() {
 }
 template <typename T>
 void TPQueue<T>::push(const T & item) {
-    assert(count < size);
-    int prior = 0;
-    int beginC = begin;
-    int inPlace = 0;
-    bool flag = 0;
-    T trans, trans2;
-    if (beginC != end) {
-        while (beginC != end) {
-            if (!(arr[beginC].prior < item.prior)) {
-                beginC++;
-                if (beginC == size)
-                    beginC = 0;
-            } else {
-                trans = arr[beginC];
-                arr[beginC] = item;
-                inPlace = beginC + 1;
-                end++;
-                count++;
-                flag = 1;
-                break;
-            }
+  assert(count < size);
+  int prior = 0;
+  int beginC = begin;
+  int inPlace = 0;
+  bool flag = 0;
+  T trans, trans2;
+  if (beginC != end) {
+    while (beginC != end) {
+        if (!(arr[beginC].prior < item.prior)) {
+          beginC++;
+          if (beginC == size)
+            beginC = 0;
+        } else {
+          trans = arr[beginC];
+          arr[beginC] = item;
+          inPlace = beginC + 1;
+          end++;
+          count++;
+          flag = 1;
+          break;
         }
-        beginC = inPlace;
-        if (flag) {
-            while (beginC != end) {
-                trans2 = arr[beginC];
-                arr[beginC] = trans;
-                trans = trans2;
-                beginC++;
-                if (beginC == size)
-                    beginC = 0;
-            }
-        }
-        if (!flag) {
-            arr[end++] = item;
-            inPlace = end;
-            count++;
-        }
-    } else {
-        arr[begin] = item;
-        count++;
-        end++;
     }
-    if (end > size)
-        end -= size + 1;
+    beginC = inPlace;
+    if (flag) {
+        while (beginC != end) {
+          trans2 = arr[beginC];
+          arr[beginC] = trans;
+          trans = trans2;
+          beginC++;
+            if (beginC == size)
+              beginC = 0;
+        }
+    }
+    if (!flag) {
+      arr[end++] = item;
+      inPlace = end;
+      count++;
+    }
+  } else {
+    arr[begin] = item;
+    count++;
+    end++;
+  }
+  if (end > size)
+    end -= size + 1;
 }
 template<typename T>
 T TPQueue<T>::pop() {
-    assert(count > 0);
-    T item = arr[begin++];
-    count--;
-    if (begin > size)
-        begin -= size + 1;
-    return item;
+  assert(count > 0);
+  T item = arr[begin++];
+  count--;
+  if (begin > size)
+    begin -= size + 1;
+  return item;
 }
 template<typename T>
 T TPQueue<T>::get() const {
-    assert(count > 0);
-    return arr[begin];
+  assert(count > 0);
+  return arr[begin];
 }
 template<typename T>
 bool TPQueue<T>::isEmpty() const {
